913bd995b4b1d1037a28daff654ad48414d70c84
MuhammadA-C <phantomganhdi@gmail.com>
2023-10-06 22:20:09 -0700

    File Setup
    Created the files needed for the app.

--- Performance-Assessment/app/src/main/java/com/zybooks/muhammadchambersc196pa/UI/HomeScreen.java
+++ Performance-Assessment/app/src/main/java/com/zybooks/muhammadchambersc196pa/UI/HomeScreen.java
@@ -1,14 +1,16 @@
-package com.zybooks.muhammadchambersc196pa;
+package com.zybooks.muhammadchambersc196pa.UI;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.zybooks.muhammadchambersc196pa.R;
+
+public class HomeScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_home_screen);
     }
 }
---
+++ Performance-Assessment/app/src/main/java/com/zybooks/muhammadchambersc196pa/UI/ListOfCourses.java
@@ -0,0 +1,16 @@
+package com.zybooks.muhammadchambersc196pa.UI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.zybooks.muhammadchambersc196pa.R;
+
+public class ListOfCourses extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_of_courses);
+    }
+}

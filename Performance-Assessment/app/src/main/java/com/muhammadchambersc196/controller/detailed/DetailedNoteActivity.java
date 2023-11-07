7bf5fe3cbe2fc6cb4fbda12b52d2e803c4bc8bbd
MuhammadA-C <phantomganhdi@gmail.com>
2023-11-06 20:57:46 -0800

    Share Note
    Created a detailed note view that allows the user to share or edit the note.

---
+++ Performance-Assessment/app/src/main/java/com/muhammadchambersc196/controller/detailed/DetailedNoteActivity.java
@@ -0,0 +1,16 @@
+package com.muhammadchambersc196.controller.detailed;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.muhammadchambersc196.R;
+
+public class DetailedNoteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detailed_note);
+    }
+}
